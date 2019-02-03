b6afb22230761da2bd9cfb635af5237307fb1a07
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-03 14:14:56 -0800

    react test deploy

---
+++ build/precache-manifest.667d080e7efde4fd5bc395e24a003f9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d171256e4e2f55b06e2",
+    "url": "/clickgame/static/css/main.fabed647.chunk.css"
+  },
+  {
+    "revision": "3d171256e4e2f55b06e2",
+    "url": "/clickgame/static/js/main.3d171256.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickgame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1d1da0236ccd610ca49c",
+    "url": "/clickgame/static/js/runtime~main.1d1da023.js"
+  },
+  {
+    "revision": "bf1a66e82a8ed4571fa6806e7a6df78a",
+    "url": "/clickgame/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -29,22 +29,22 @@ class App extends Component {
     return true;
   }
 
-  clickCount = id => {
-    this.state.friends.find((o, i) => {
-      if (o.id === id) {
-        if(friends[i].count === 0){
-          friends[i].count = friends[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
-          });
-          this.state.friends.sort(() => Math.random() - 0.5)
-          return true; 
-        } else {
-          this.gameOver();
-        }
-      }
-    });
-  }
+  // clickCount = id => {
+  //   this.state.friends.find((o, i) => {
+  //     if (o.id === id) {
+  //       if(friends[i].count === 0){
+  //         friends[i].count = friends[i].count + 1;
+  //         this.setState({score : this.state.score + 1}, function(){
+  //           console.log(this.state.score);
+  //         });
+  //         this.state.friends.sort(() => Math.random() - 0.5)
+  //         return true; 
+  //       } else {
+  //         this.gameOver();
+  //       }
+  //     }
+  //   });
+  // }
 
 
   render() {
@@ -52,7 +52,7 @@ class App extends Component {
     return (
 
       <Wrapper>
-      <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+      <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game  </Header>
       {this.state.friends.map(card => (
         <Card
           clickCount={this.clickCount}
