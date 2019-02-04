85b150968b169e1afd137ee5fc36d699963a2b1c
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-04 14:19:30 -0800

    Updates

---
+++ precache-manifest.7e4f1f6fea542b27603ccf4fdcbc9cfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eff49649afc9bc7b7a2",
+    "url": "/clickgame/static/css/main.fabed647.chunk.css"
+  },
+  {
+    "revision": "9eff49649afc9bc7b7a2",
+    "url": "/clickgame/static/js/main.9eff4964.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clickgame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "1d1da0236ccd610ca49c",
+    "url": "/clickgame/static/js/runtime~main.1d1da023.js"
+  },
+  {
+    "revision": "8ba353902094744cfbf4793472220d37",
+    "url": "/clickgame/index.html"
+  }
+];
